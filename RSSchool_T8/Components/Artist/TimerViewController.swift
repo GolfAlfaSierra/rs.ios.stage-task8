572de0baf94be2543162ed4fdef69fd1e12cdd80
Artyom Sog <tomas.tomson@Mail.ru>
2021-07-19 20:20:47 +0300

    timer view made

--- RSSchool_T8/Components/Artist/TimerViewController.swift
+++ RSSchool_T8/Components/Artist/TimerViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 public class TimerViewController: UIViewController {
  
+    @IBOutlet weak var saveTimer: UIButton!
+    
+    public override func viewDidLoad() {
+        self.view.layer.cornerRadius = 40
+        self.view.layer.shadowOffset = CGSize.zero
+        self.view.layer.shadowOpacity = 0.25
+        self.view.layer.shadowColor = UIColor.black.cgColor
+        self.view.layer.shadowRadius = 8
+        
+        saveTimer.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
+        
+    }
+    
+    @objc func dismissView() {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
 
 extension TimerViewController: UIViewControllerTransitioningDelegate {
